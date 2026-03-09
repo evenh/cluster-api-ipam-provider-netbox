6c74cf2f6c77f1f22d04a40a87725d32e3e34e71
Even Holthe <even.holthe@bekk.no>
2026-03-10 00:37:22 +0100

    feat: run metrics endpoint insecurely

--- cmd/main.go
+++ cmd/main.go
@@ -17,15 +17,16 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/spf13/pflag"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/pkg/version"
 	"k8s.io/klog/v2"
@@ -35,7 +36,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
@@ -45,153 +45,199 @@ import (
 	ipamutil "github.com/evenh/cluster-api-ipam-provider-netbox/pkg/ipamutil"
 )
 
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
+const (
+	defaultMetricsAddr = "0"
+	defaultProbeAddr   = ":8081"
 )
 
-func init() {
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-	utilruntime.Must(clusterv1.AddToScheme(scheme))
-	utilruntime.Must(ipamv1.AddToScheme(scheme))
-	utilruntime.Must(ipamv1alpha1.AddToScheme(scheme))
+type managerConfig struct {
+	metricsAddr          string
+	webhookCertPath      string
+	webhookCertName      string
+	webhookCertKey       string
+	enableLeaderElection bool
+	probeAddr            string
+	watchNamespace       string
+	watchFilter          string
+	enableHTTP2          bool
+	managerOptions       capiflags.ManagerOptions
 }
 
 func main() {
-	var (
-		metricsAddr          string
-		metricsCertPath      string
-		metricsCertName      string
-		metricsCertKey       string
-		webhookCertPath      string
-		webhookCertName      string
-		webhookCertKey       string
-		enableLeaderElection bool
-		probeAddr            string
-		watchNamespace       string
-		watchFilter          string
-		secureMetrics        bool
-		enableHTTP2          bool
-		tlsOpts              []func(*tls.Config)
-		managerOptions       = capiflags.ManagerOptions{}
-	)
-
-	capiflags.AddManagerOptions(pflag.CommandLine, &managerOptions)
-
-	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "Metrics bind address. Use 0 to disable.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "Health probe bind address.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false, "Enable leader election.")
-	flag.StringVar(&watchNamespace, "namespace", "", "Namespace to watch. Empty means all namespaces.")
-	flag.StringVar(&watchFilter, "watch-filter", "", "Cluster API watch filter label value.")
-	flag.BoolVar(&secureMetrics, "metrics-secure", true, "Serve metrics over HTTPS.")
-	flag.StringVar(&webhookCertPath, "webhook-cert-path", "", "Webhook certificate directory.")
-	flag.StringVar(&webhookCertName, "webhook-cert-name", "tls.crt", "Webhook certificate file.")
-	flag.StringVar(&webhookCertKey, "webhook-cert-key", "tls.key", "Webhook private key file.")
-	flag.StringVar(&metricsCertPath, "metrics-cert-path", "", "Metrics certificate directory.")
-	flag.StringVar(&metricsCertName, "metrics-cert-name", "tls.crt", "Metrics certificate file.")
-	flag.StringVar(&metricsCertKey, "metrics-cert-key", "tls.key", "Metrics private key file.")
-	flag.BoolVar(&enableHTTP2, "enable-http2", false, "Enable HTTP/2 for metrics and webhooks.")
+	cfg := parseFlags()
 
 	goFlagSet := flag.CommandLine
 	pflag.CommandLine.AddGoFlagSet(goFlagSet)
 	pflag.Parse()
 
 	ctrl.SetLogger(klog.Background())
+	setupLog := ctrl.Log.WithName("setup")
 
-	if !enableHTTP2 {
-		tlsOpts = append(tlsOpts, func(c *tls.Config) {
-			c.NextProtos = []string{"http/1.1"}
-		})
-	}
-
-	webhookServerOptions := webhook.Options{TLSOpts: tlsOpts}
-	if webhookCertPath != "" {
-		webhookServerOptions.CertDir = webhookCertPath
-		webhookServerOptions.CertName = webhookCertName
-		webhookServerOptions.KeyName = webhookCertKey
+	scheme, err := newScheme()
+	if err != nil {
+		setupLog.Error(err, "Failed to build scheme")
+		os.Exit(1)
 	}
 
-	metricsServerOptions := metricsserver.Options{
-		BindAddress:   metricsAddr,
-		SecureServing: secureMetrics,
-		TLSOpts:       tlsOpts,
-	}
-	if secureMetrics {
-		metricsServerOptions.FilterProvider = filters.WithAuthenticationAndAuthorization
-	}
-	if metricsCertPath != "" {
-		metricsServerOptions.CertDir = metricsCertPath
-		metricsServerOptions.CertName = metricsCertName
-		metricsServerOptions.KeyName = metricsCertKey
-	}
+	tlsOpts := makeTLSOptions(cfg.enableHTTP2)
+	webhookServerOptions := newWebhookServerOptions(cfg, tlsOpts)
+	managerOpts := newManagerOptions(cfg, scheme, webhookServerOptions)
 
-	options := ctrl.Options{
-		Scheme:                 scheme,
-		Metrics:                metricsServerOptions,
-		WebhookServer:          webhook.NewServer(webhookServerOptions),
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "cluster-api-ipam-provider-netbox.ipam.cluster.x-k8s.io",
-	}
-	if watchNamespace != "" {
-		options.Cache = cache.Options{
-			DefaultNamespaces: map[string]cache.Config{
-				watchNamespace: {},
-			},
-		}
-	}
-
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), options)
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), managerOpts)
 	if err != nil {
 		setupLog.Error(err, "Failed to start manager")
 		os.Exit(1)
 	}
 
 	ctx := ctrl.SetupSignalHandler()
-
-	if err := index.SetupIndexes(ctx, mgr); err != nil {
+	if err = index.SetupIndexes(ctx, mgr); err != nil {
 		setupLog.Error(err, "Failed to set up indexes")
 		os.Exit(1)
 	}
+	if err = setupControllers(ctx, mgr, cfg.watchFilter); err != nil {
+		setupLog.Error(err, "Failed to set up controllers")
+		os.Exit(1)
+	}
+	if err = addProbes(mgr); err != nil {
+		setupLog.Error(err, "Failed to set up probes")
+		os.Exit(1)
+	}
 
+	setupLog.Info("Starting manager", "version", version.Get().String())
+	if err = mgr.Start(ctx); err != nil {
+		setupLog.Error(err, "Failed to run manager")
+		os.Exit(1)
+	}
+}
+
+func parseFlags() managerConfig {
+	cfg := managerConfig{
+		metricsAddr:     defaultMetricsAddr,
+		probeAddr:       defaultProbeAddr,
+		webhookCertName: "tls.crt",
+		webhookCertKey:  "tls.key",
+		managerOptions:  capiflags.ManagerOptions{},
+	}
+
+	capiflags.AddManagerOptions(pflag.CommandLine, &cfg.managerOptions)
+	flag.StringVar(
+		&cfg.metricsAddr,
+		"metrics-bind-address",
+		defaultMetricsAddr,
+		"Metrics bind address. Use 0 to disable.",
+	)
+	flag.StringVar(&cfg.probeAddr, "health-probe-bind-address", defaultProbeAddr, "Health probe bind address.")
+	flag.BoolVar(&cfg.enableLeaderElection, "leader-elect", false, "Enable leader election.")
+	flag.StringVar(&cfg.watchNamespace, "namespace", "", "Namespace to watch. Empty means all namespaces.")
+	flag.StringVar(&cfg.watchFilter, "watch-filter", "", "Cluster API watch filter label value.")
+	flag.StringVar(&cfg.webhookCertPath, "webhook-cert-path", "", "Webhook certificate directory.")
+	flag.StringVar(&cfg.webhookCertName, "webhook-cert-name", cfg.webhookCertName, "Webhook certificate file.")
+	flag.StringVar(&cfg.webhookCertKey, "webhook-cert-key", cfg.webhookCertKey, "Webhook private key file.")
+	flag.BoolVar(&cfg.enableHTTP2, "enable-http2", false, "Enable HTTP/2 for metrics and webhooks.")
+
+	return cfg
+}
+
+func newScheme() (*runtime.Scheme, error) {
+	scheme := runtime.NewScheme()
+	for _, add := range []func(*runtime.Scheme) error{
+		clientgoscheme.AddToScheme,
+		clusterv1.AddToScheme,
+		ipamv1.AddToScheme,
+		ipamv1alpha1.AddToScheme,
+	} {
+		if err := add(scheme); err != nil {
+			return nil, fmt.Errorf("add types to scheme: %w", err)
+		}
+	}
+	return scheme, nil
+}
+
+func makeTLSOptions(enableHTTP2 bool) []func(*tls.Config) {
+	if enableHTTP2 {
+		return nil
+	}
+
+	return []func(*tls.Config){
+		func(c *tls.Config) {
+			c.NextProtos = []string{"http/1.1"}
+		},
+	}
+}
+
+func newWebhookServerOptions(cfg managerConfig, tlsOpts []func(*tls.Config)) webhook.Options {
+	webhookServerOptions := webhook.Options{TLSOpts: tlsOpts}
+	if cfg.webhookCertPath != "" {
+		webhookServerOptions.CertDir = cfg.webhookCertPath
+		webhookServerOptions.CertName = cfg.webhookCertName
+		webhookServerOptions.KeyName = cfg.webhookCertKey
+	}
+
+	return webhookServerOptions
+}
+
+func newManagerOptions(
+	cfg managerConfig,
+	scheme *runtime.Scheme,
+	webhookServerOptions webhook.Options,
+) ctrl.Options {
+	metricsServerOptions := metricsserver.Options{
+		BindAddress: cfg.metricsAddr,
+	}
+
+	options := ctrl.Options{
+		Scheme:                 scheme,
+		Metrics:                metricsServerOptions,
+		WebhookServer:          webhook.NewServer(webhookServerOptions),
+		HealthProbeBindAddress: cfg.probeAddr,
+		LeaderElection:         cfg.enableLeaderElection,
+		LeaderElectionID:       "cluster-api-ipam-provider-netbox.ipam.cluster.x-k8s.io",
+	}
+	if cfg.watchNamespace != "" {
+		options.Cache = cache.Options{
+			DefaultNamespaces: map[string]cache.Config{
+				cfg.watchNamespace: {},
+			},
+		}
+	}
+
+	return options
+}
+
+func setupControllers(ctx context.Context, mgr ctrl.Manager, watchFilter string) error {
 	if err := (&ipamutil.ClaimReconciler{
 		Client:           mgr.GetClient(),
 		Scheme:           mgr.GetScheme(),
 		WatchFilterValue: watchFilter,
 		Adapter:          &controller.NetBoxProviderAdapter{},
 	}).SetupWithManager(ctx, mgr); err != nil {
-		setupLog.Error(err, "Failed to create IPAddressClaim reconciler")
-		os.Exit(1)
+		return fmt.Errorf("create IPAddressClaim reconciler: %w", err)
 	}
 
 	if err := (&controller.NetBoxIPPoolReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "Failed to create NetBoxIPPool reconciler")
-		os.Exit(1)
+		return fmt.Errorf("create NetBoxIPPool reconciler: %w", err)
 	}
 
 	if err := (&controller.GlobalNetBoxIPPoolReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "Failed to create GlobalNetBoxIPPool reconciler")
-		os.Exit(1)
+		return fmt.Errorf("create GlobalNetBoxIPPool reconciler: %w", err)
 	}
 
+	return nil
+}
+
+func addProbes(mgr ctrl.Manager) error {
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-		setupLog.Error(err, "Failed to set up health check")
-		os.Exit(1)
+		return fmt.Errorf("set up health check: %w", err)
 	}
 	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-		setupLog.Error(err, "Failed to set up ready check")
-		os.Exit(1)
+		return fmt.Errorf("set up ready check: %w", err)
 	}
 
-	setupLog.Info("Starting manager", "version", version.Get().String())
-	if err := mgr.Start(ctx); err != nil {
-		setupLog.Error(err, "Failed to run manager")
-		os.Exit(1)
-	}
+	return nil
 }
